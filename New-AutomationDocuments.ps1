471774ddbb4261f82fa3e1ce140242ba3afeb682
Emmanuel Tsouris <emmanuel@tsouris.com>
2018-10-12 16:20:57 -0700

    Rename documents

--- New-AutomationDocuments.ps1
+++ New-AutomationDocuments.ps1
@@ -5,32 +5,32 @@ Set-DefaultAWSRegion -Region us-east-1
 # Automation Document to build a Developer Windows Image
 
 # Load the content from the json file
-$developerImageJson = Get-Content .\AutomationDocuments\Build-DeveloperImage.json -Raw
+$developerImageJson = Get-Content .\AutomationDocuments\Demo-CreateDeveloperImage.json -Raw
 
 # Create a new document, specifying Automation as the DocumentType and passing the content
-New-SSMDocument -Name "Build-DeveloperImage" -Content $developerImageJson -DocumentType Automation
+New-SSMDocument -Name "Demo-CreateDeveloperImage" -Content $developerImageJson -DocumentType Automation
 
 <#
     # If the document exists, use the update cmdlet
-    Update-SSMDocument -Content $developerImageJson -Name "Build-DeveloperImage" -DocumentVersion '$LATEST'
+    Update-SSMDocument -Content $developerImageJson -Name "Demo-CreateDeveloperImage" -DocumentVersion '$LATEST'
 
     # To remove the document
-    Remove-SSMDocument -Name "Build-DeveloperImage" -Force
+    Remove-SSMDocument -Name "Demo-CreateDeveloperImage" -Force
 #>
 
 ##############################################################
 # Automation Document to build a SQL DBA Windows Image
 
 # Load the content from the json file
-$sqlImageJson = Get-Content .\AutomationDocuments\Build-SqlDbaImage.json -Raw
+$sqlImageJson = Get-Content .\AutomationDocuments\Demo-CreateSqlDevImage.json -Raw
 
 # Create a new document, specifying Automation as the DocumentType and passing the content
-New-SSMDocument -Name "Build-SqlDbaImage" -Content $sqlImageJson -DocumentType Automation
+New-SSMDocument -Name "Demo-CreateSqlDevImage" -Content $sqlImageJson -DocumentType Automation
 
 <#
     # If the document exists, use the update cmdlet
-    Update-SSMDocument -Content $sqlImageJson -Name "Build-SqlDbaImage" -DocumentVersion '$LATEST'
+    Update-SSMDocument -Content $sqlImageJson -Name "Demo-CreateSqlDevImage" -DocumentVersion '$LATEST'
 
     # To remove the document
-    Remove-SSMDocument -Name "Build-SqlDbaImage" -Force
+    Remove-SSMDocument -Name "Demo-CreateSqlDevImage" -Force
 #>
