68cc4e3865f633a06a01761e065137318123e4f1
Emmanuel Tsouris <emmanuel@tsouris.com>
2018-10-12 16:38:34 -0700

    Adding code to check CloudWatch Logs

--- Start-AutomationDocument.ps1
+++ Start-AutomationDocument.ps1
@@ -3,6 +3,19 @@ Set-DefaultAWSRegion -Region us-east-1
 
 # Kick off the automation documents which will create the images
 
-Start-SSMAutomationExecution -DocumentName "Demo-CreateDeveloperImage"
+$developerImageExecId = Start-SSMAutomationExecution -DocumentName "Demo-CreateDeveloperImage"
+$sqlDevImageExecId = Start-SSMAutomationExecution -DocumentName "Demo-CreateSqlDevImage"
 
-Start-SSMAutomationExecution -DocumentName "Demo-CreateSqlDevImage"
+# Get Automation Execution Status
+Get-SSMAutomationExecution -AutomationExecutionId $developerImageExecId
+Get-SSMAutomationExecution -AutomationExecutionId $sqlDevImageExecId
+
+
+# Get the CloudWatch Log Group Name from Parameter Store
+$cwlGroupName = (Get-SSMParameter -Name "/Test/AutomationDemo/CloudWatchLogGroup").Value
+
+# Get the CloudWatch Log Streams, and just select the newest one.
+$cwlStreams = Get-CWLLogStreams -LogGroupName $cwlGroupName | Sort-Object CreationTime -Descending | Select-Object -First 1
+
+# Show the events
+($cwlStreams | Get-CWLLogEvent -LogGroupName $cwlGroupName).Events
