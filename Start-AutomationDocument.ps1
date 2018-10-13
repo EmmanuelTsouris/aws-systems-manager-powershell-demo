64d0de715d3a5bd3731c5e13b2da9dbf0779c5b7
Emmanuel Tsouris <emmanuel@tsouris.com>
2018-10-13 12:48:57 -0700

    Added ImageName param to call

--- Start-AutomationDocument.ps1
+++ Start-AutomationDocument.ps1
@@ -3,13 +3,20 @@ Set-DefaultAWSRegion -Region us-east-1
 
 # Kick off the automation documents which will create the images
 
-$developerImageExecId = Start-SSMAutomationExecution -DocumentName "Demo-CreateDeveloperImage"
-$sqlDevImageExecId = Start-SSMAutomationExecution -DocumentName "Demo-CreateSqlDevImage"
+# First, we'll create a hashtable representing the parameters from the Automation document.
+# In this case we'll set the ImageName instead of taking the parameter default defined in the document.
+$devImageParams = @{'ImageName' = "Windows_Server-2016-Developer"}
+
+# Kick off the automation and pass the parameter we defined above.
+$developerImageExecId = Start-SSMAutomationExecution -DocumentName "Demo-CreateDeveloperImage" -Parameter $devImageParams
+
+# Parameter for our second image, giving it a different name.
+$sqlImageParams = @{'ImageName' = "Windows_Server-2016-SQLDeveloper"}
+$sqlDevImageExecId = Start-SSMAutomationExecution -DocumentName "Demo-CreateSqlDevImage" -Parameter $sqlImageParams
 
 # Get Automation Execution Status
 Get-SSMAutomationExecution -AutomationExecutionId $developerImageExecId
 Get-SSMAutomationExecution -AutomationExecutionId $sqlDevImageExecId
-
 
 # Get the CloudWatch Log Group Name from Parameter Store
 $cwlGroupName = (Get-SSMParameter -Name "/Test/AutomationDemo/CloudWatchLogGroup").Value
