2231763e28747973b08df40111203a6ffe616a76
Emmanuel Tsouris <emmanuel@tsouris.com>
2018-10-13 13:09:23 -0700

    Added date suffix to ImageName

--- Start-AutomationDocument.ps1
+++ Start-AutomationDocument.ps1
@@ -3,15 +3,15 @@ Set-DefaultAWSRegion -Region us-east-1
 
 # Kick off the automation documents which will create the images
 
+$dateSuffix = Get-Date -UFormat "%Y.%m.%d-%H.%M.%S"
+
 # First, we'll create a hashtable representing the parameters from the Automation document.
 # In this case we'll set the ImageName instead of taking the parameter default defined in the document.
-$devImageParams = @{'ImageName' = "Windows_Server-2016-Developer"}
+$devImageParams = @{'ImageName' = "Windows_Server-2016-Developer-$dateSuffix"}
+$sqlImageParams = @{'ImageName' = "Windows_Server-2016-SQLDeveloper-$dateSuffix"}
 
 # Kick off the automation and pass the parameter we defined above.
 $developerImageExecId = Start-SSMAutomationExecution -DocumentName "Demo-CreateDeveloperImage" -Parameter $devImageParams
-
-# Parameter for our second image, giving it a different name.
-$sqlImageParams = @{'ImageName' = "Windows_Server-2016-SQLDeveloper"}
 $sqlDevImageExecId = Start-SSMAutomationExecution -DocumentName "Demo-CreateSqlDevImage" -Parameter $sqlImageParams
 
 # Get Automation Execution Status
